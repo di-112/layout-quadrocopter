bd9666c3fd26b5e7f63dddc92d6e92f5a75e7043
di-112 <73027626+di-112@users.noreply.github.com>
2021-03-07 00:00:36 +0300

    added all sections

---
+++ js/slick.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 
+$('.slider').slick(
+ {
+   arrows:false,
+   dots:true,
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   speed: 500,
+   vertical: true,
+   draggable: false,
+   autoplay: true,
+   autoplaySpeed: 5000,
+ }
+) 
+})
